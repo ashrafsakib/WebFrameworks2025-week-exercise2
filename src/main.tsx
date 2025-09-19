1574c9244e166bf650b0d28801e2eb02aecf6f66
Syed Md Afsani <68096636+smafsani@users.noreply.github.com>
2025-09-20 00:33:36 +0300

    Completed exercise 2

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const courses = [
+  { name: "Course 1", courseId: "ABC123", studentPositions: 30 },
+  { name: "Course 2", courseId: "DEF456", studentPositions: 25 },
+  { name: "Course 3", courseId: "GHI789", studentPositions: 20 },
+];
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App courses={courses} />
   </StrictMode>,
 )
